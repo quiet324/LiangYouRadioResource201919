2d4f9c144c6e575b2a6e4151b36234759d36f15f
Paul <510830568@qq.com>
2019-05-05 05:05:45 -0400

    Auto-commit

--- node/everyday/all-week-after-week-cos-and-qiniu-do-not-upload-no-github.js
+++ node/everyday/all-week-after-week-cos-and-qiniu-do-not-upload-no-github.js
@@ -15,8 +15,8 @@ var deasync = require('deasync');
 
 var rule = new schedule.RecurrenceRule();
 // rule.dayOfWeek = [0, new schedule.Range(4, 6)];
-rule.hour = [1, 2, 6, 9, 15, 17, 21];
-rule.minute = 49;
+rule.hour = [1, 2, 6, 9, 17, 21];
+rule.minute = 4;
 
 
 
